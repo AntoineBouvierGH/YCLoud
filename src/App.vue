0b2c28ac6af470f4f41ff49c03582d6e341f0fdd
Antoine Bouvier <antoine.bouvier@epitech.eu>
2022-07-29 16:55:47 +0200

    add file component

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <FilesView></FilesView>
+</template>
+
+<script>
+import FilesView from "./views/FilesView.vue";
+
+export default {
+  name: "App",
+  components: {
+    FilesView,
+  },
+};
+</script>
+
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0;
+  padding: 0;
+  background-color: green;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  background-color: red;
+}
+</style>
---
+++ src/views/FilesView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="files">
+    <h4>{{ path }}</h4>
+    <div class="list" v-for="content in folderContent" v-bind:key="content">
+      <span>{{ content }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "FilesView",
+  props: {},
+  data() {
+    return {
+      folderContent: [],
+      path: "",
+    };
+  },
+  mounted() {
+    console.log("hehe");
+    axios.get("http://localhost:3000/api/files").then((res) => {
+      console.log(res);
+      this.folderContent = res.data.content;
+    });
+
+    axios.get("http://localhost:3000/api/files/location").then((res) => {
+      console.log(res);
+      this.path = res.data.path;
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
